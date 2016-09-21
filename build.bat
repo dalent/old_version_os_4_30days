d31c808bb982da119c07c1e748e4d23f205cbf05
jujintao <jujintao@jk.cn>
2016-09-21 19:06:15 +0800

    add new version

--- build.bat
+++ build.bat
@@ -2,24 +2,24 @@ del /Q .\bin\*.*
 nasm -o .\bin\begin.bin begin.asm
 nasm -o .\bin\haribote.o haribote.asm
 nasm -f aout -o .\bin\kernelFunc kernelfunc.asm
-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\bootpack bootpack.c 
-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\gdt gdt.c
-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\graphic graphic.c
-gcc -Wall    -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\idt idt.c
-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\pic pic.c
-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\mouse mouse.c
-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\fifo fifo.c
-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\kb kb.c
-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\memory memory.c
-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\unity unity.c
-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\s sheet.c
-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\t timer.c
-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\m mtask.c
-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\f file.c
-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\c console.c
-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\w window.c
+i586-pc-msdosdjgpp-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\bootpack bootpack.c 
+i586-pc-msdosdjgpp-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\gdt gdt.c
+i586-pc-msdosdjgpp-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\graphic graphic.c
+i586-pc-msdosdjgpp-gcc -Wall    -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\idt idt.c
+i586-pc-msdosdjgpp-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\pic pic.c
+i586-pc-msdosdjgpp-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\mouse mouse.c
+i586-pc-msdosdjgpp-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\fifo fifo.c
+i586-pc-msdosdjgpp-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\kb kb.c
+i586-pc-msdosdjgpp-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\memory memory.c
+i586-pc-msdosdjgpp-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\unity unity.c
+i586-pc-msdosdjgpp-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\s sheet.c
+i586-pc-msdosdjgpp-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\t timer.c
+i586-pc-msdosdjgpp-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\m mtask.c
+i586-pc-msdosdjgpp-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\f file.c
+i586-pc-msdosdjgpp-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\c console.c
+i586-pc-msdosdjgpp-gcc -Wall   -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin  -c -o .\bin\w window.c
 cd .\bin 
-ld -T .\..\link.ld -o a kernelfunc bootpack .\..\hankaku.obj gdt idt graphic pic mouse fifo kb memory unity s t m f c w
+i586-pc-msdosdjgpp-ld -T .\..\link.ld  -o a kernelfunc bootpack .\..\hankaku.obj gdt idt graphic pic mouse fifo kb memory unity s t m f c w
 copy /B haribote.o+a kernel  
 
 
--- readme.txt
+++ readme.txt
@@ -1,6 +1,18 @@
-1. please install djgpp nasm  first
-2. begin.asm is the bootloader
-3. haribote is the bios to 32bit system.(16 bit real mode to protect mode)
-4.kernelFunc.asm is our assembly entry and a lot of useful function
-5. bootpack.c is our main entry.(i used hello)
-6. others is like the file name.
+1.if win7 64bit  please install nasm and cygwin (select the devel/djgpp-binutils gcc-core)
+1.if below win7 64bit please install djgpp nasm  first(and modify the build.bat use standard gcc ld)
+2. please install winimage 
+3.build.bat build it check if success 
+4. use winimage add new 1.44M image set the image/boot sector properties to bin/bootload.bin
+5.use winimage image/inject to inject the bin/kernel save to fg: a.ima
+6. use virtualbox to add new linux 32 system , add the new floppy disk a.ima
+7.start it 
+
+
+explain:
+
+0. begin.asm is the bootloader
+1. haribote is the bios to 32bit system.(16 bit real mode to protect mode)
+2.kernelFunc.asm is our assembly entry and a lot of useful function
+3. bootpack.c is our main entry.(i used hello)
+4. others is like the file name.
+5. 4.ima is a example , start it with virtual box and type "dir" , then type  "start invader"
